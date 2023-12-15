f5d9259425bdf83b37f61a87be5c96333e9b1cd0
Agustinus Henan <93471926+agustinushenan01@users.noreply.github.com>
2023-12-15 21:49:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,82 +1,87 @@
 const imageNames = [
-            "tobat.jpg",
-            "sarung bantal.png",
-            "tabung air.png",
-            "tanta.png",
-            "satu kampung.png"
-        ];
+    "tobat.jpg",
+    "sarung bantal.png",
+    "tabung air.png",
+    "tanta.png",
+    "satu kampung.png",
+    "sayur bayam.png",
+    "sapu tangan.png",
+    "rebus ikan.png",
+    "kereta listrik.png",
+    "jam kerja.png"
+];
 
-        let correctAnswers = imageNames.map(name => name.split(".")[0].toUpperCase());
-        let remainingHearts = 3;
-        let initialHearts = remainingHearts;
-        let currentQuestionIndex = 0;
+let correctAnswers = imageNames.map(name => name.split(".")[0].toUpperCase());
+let remainingHearts = 3;
+let initialHearts = remainingHearts;
+let currentQuestionIndex = 0;
 
-        function checkAnswer() {
-            const userAnswer = document.getElementById("answerInput").value.toUpperCase();
-            const correctAnswer = correctAnswers[currentQuestionIndex];
+function checkAnswer() {
+    const userAnswer = document.getElementById("answerInput").value.toUpperCase();
+    const correctAnswer = correctAnswers[currentQuestionIndex];
 
-            if (userAnswer === correctAnswer) {
-                // Jawaban benar
-                document.getElementById("answerInput").style.display = "none";
-                document.querySelector("#guessForm button").style.display = "none";
-                document.getElementById("nextButton").style.display = "block";
-            } else {
-                // Jawaban salah
-                remainingHearts--;
+    if (userAnswer === correctAnswer) {
+        // Jawaban benar
+        document.getElementById("answerInput").style.display = "none";
+        document.querySelector("#guessForm button").style.display = "none";
+        document.getElementById("nextButton").style.display = "block";
+    } else {
+        // Jawaban salah
+        remainingHearts--;
 
-                if (remainingHearts === 0) {
-                    gameOver();
-                } else {
-                    document.getElementById("heart" + remainingHearts).style.display = "none";
-                }
-            }
+        if (remainingHearts === 0) {
+            gameOver();
+        } else {
+            document.getElementById("heart" + remainingHearts).style.display = "none";
         }
+    }
+}
 
-        function gameOver() {
-            alert("Game Over! Kamu kalah.");
-            resetGame();
-        }
+function gameOver() {
+    alert("Game Over! Kamu kalah.");
+    resetGame();
+}
 
-        function resetGame() {
-            currentQuestionIndex = 0;
-            remainingHearts = initialHearts;
-            resetState();
-            showQuestion();
-        }
+function resetGame() {
+    currentQuestionIndex = 0;
+    remainingHearts = initialHearts;
+    resetState();
+    showQuestion();
+}
 
-        function nextQuestion() {
-            currentQuestionIndex++;
+function nextQuestion() {
+    currentQuestionIndex++;
 
-            if (currentQuestionIndex < imageNames.length) {
-                resetState();
-                showQuestion();
-            } else {
-                alert("Selamat! Kamu berhasil.");
-                // Tambahkan logika lain yang ingin Anda lakukan ketika permainan selesai
-            }
-        }
+    if (currentQuestionIndex < imageNames.length) {
+        resetState();
+        showQuestion();
+    } else {
+        alert("Selamat! Kamu berhasil.");
+        // Tambahkan logika lain yang ingin Anda lakukan ketika permainan selesai
+    }
+}
 
-        function resetState() {
-            if (remainingHearts === 0) {
-                remainingHearts = initialHearts;
-            }
+function resetState() {
+    if (remainingHearts === 0) {
+        remainingHearts = initialHearts;
+    }
 
-            for (let i = 1; i <= initialHearts; i++) {
-                const heartId = "heart" + i;
-                document.getElementById(heartId).style.display = i <= remainingHearts ? "inline-block" : "none";
-            }
+    for (let i = 1; i <= initialHearts; i++) {
+        const heartId = "heart" + i;
+        document.getElementById(heartId).style.display = i <= remainingHearts ? "inline-block" : "none";
+    }
 
-            document.getElementById("answerInput").style.display = "block";
-            document.querySelector("#guessForm button").style.display = "block";
-            document.getElementById("nextButton").style.display = "none";
-            document.getElementById("answerInput").value = "";
-        }
+    document.getElementById("answerInput").style.display = "block";
+    document.querySelector("#guessForm button").style.display = "block";
+    document.getElementById("nextButton").style.display = "none";
+    document.getElementById("answerInput").value = "";
+}
 
-        function showQuestion() {
-            document.getElementById("questionImage").src = `asset/${imageNames[currentQuestionIndex]}`;
-        }
+function showQuestion() {
+    document.getElementById("questionImage").src = `asset/${imageNames[currentQuestionIndex]}`;
+}
 
-        // Panggil resetGame saat memulai permainan
-        function startQuiz() {
-            resetGame();
-  }
+// Panggil resetGame saat memulai permainan
+function startQuiz() {
+    resetGame();
+}
